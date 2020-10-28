e413a12c127d6b4c68e5fdded9e244e0345ddc76
Andrey <andrey949495@yandex.ru>
2020-10-28 16:28:23 +0400

    feat: lobby, join and leave team

---
+++ src/components/team-room/team-room-players/team-room-players.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { JoinedTeamInterface } from '../../../interfaces/team.interfaces';
+import styled from 'styled-components';
+
+export interface TeamRoomPlayersProps {
+  players: JoinedTeamInterface['players'];
+}
+
+const StyledWrapper = styled.div`
+  background: ${props => props.theme.colors.secondary};
+`;
+
+const TeamRoomPlayers: React.FC<TeamRoomPlayersProps> = ({
+  players,
+}): React.ReactElement => {
+  return (
+    <StyledWrapper>
+    </StyledWrapper>
+  );
+};
+
+export default TeamRoomPlayers;

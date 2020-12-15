e90479b2e21bd0bf5e92e9c5a6617e46526cfc18
Chipsa12 <ivango23456@gmail.com>
2020-12-15 19:25:42 +0400

    create mapGame

--- application/modules/game/Game.py
+++ application/modules/game/Game.py
@@ -7,7 +7,7 @@ class Game:
     def __init__(self):
 
         self.__ships = {}
-        self.__map = [
+        self.__mapShip = [
             [0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 2, 1, 1, 2, 0, 0, 0, 0],
             [0, 0, 0, 2, 1, 1, 1, 1, 2, 0, 0, 0],
@@ -28,15 +28,16 @@ class Game:
             [0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0],
             [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0]
         ]
+        self.__mapGame = []
 
     def __randomCoord(self, furnitures):
         while True:
-            coordX = randint(0, len(self.__map) - 1)
-            coordY = randint(0, len(self.__map[coordX]) - 1)
+            coordX = randint(0, len(self.__mapShip) - 1)
+            coordY = randint(0, len(self.__mapShip[coordX]) - 1)
             for furniture in furnitures:
                 if (coordX != furniture.get()['coordX']) and \
                    (coordY != furniture.get()['coordY']) and \
-                   (self.__map[coordX][coordY] == 1):
+                   (self.__mapShip[coordX][coordY] == 1):
                     check = True
                 else:
                     check = False
@@ -52,6 +53,7 @@ class Game:
                         furniture=self.getCoordFurniture(furnitures=data['furniture'])
             ))
             self.__ships[data['id']] = ship
+            self.__mapGame.append(ship)
             return ship
         return None
 
@@ -100,29 +102,29 @@ class Game:
     def getScene(self, shipId):
         ship = self.__ships[shipId].get()
         if ship:
-            return dict(map=self.__map, ship=ship)
+            return dict(mapGame=self.__mapGame, ship=ship)
         return False
 
     def move(self, player, data):
         if data and player:
-            if data['up'] and self.__map[player['coordX']][player['coordY'] + 1] == 1:
+            if data['up'] and self.__mapShip[player['coordX']][player['coordY'] + 1] == 1:
                     player['coordY'] += 1
-            if data['down'] and self.__map[player['coordX']][player['coordY'] - 1] == 1:
+            if data['down'] and self.__mapShip[player['coordX']][player['coordY'] - 1] == 1:
                     player['coordY'] -= 1
-            if data['left'] and self.__map[player['coordX'] - 1][player['coordY']] == 1:
+            if data['left'] and self.__mapShip[player['coordX'] - 1][player['coordY']] == 1:
                     player['coordX'] -= 1
-            if data['right'] and self.__map[player['coordX'] + 1][player['coordY']] == 1:
+            if data['right'] and self.__mapShip[player['coordX'] + 1][player['coordY']] == 1:
                     player['coordX'] += 1
-            if data['up-left'] and self.__map[player['coordX'] - 1][player['coordY'] + 1] == 1:
+            if data['up-left'] and self.__mapShip[player['coordX'] - 1][player['coordY'] + 1] == 1:
                     player['coordY'] += 1
                     player['coordx'] -= 1
-            if data['up-right'] and self.__map[player['coordX'] + 1][player['coordY'] + 1] == 1:
+            if data['up-right'] and self.__mapShip[player['coordX'] + 1][player['coordY'] + 1] == 1:
                     player['coordY'] += 1
                     player['coordx'] += 1
-            if data['down-left'] and self.__map[player['coordX'] - 1][player['coordY'] - 1] == 1:
+            if data['down-left'] and self.__mapShip[player['coordX'] - 1][player['coordY'] - 1] == 1:
                     player['coordY'] -= 1
                     player['coordx'] -= 1
-            if data['down-right'] and self.__map[player['coordX'] + 1][player['coordY'] - 1] == 1:
+            if data['down-right'] and self.__mapShip[player['coordX'] + 1][player['coordY'] - 1] == 1:
                     player['coordY'] -= 1
                     player['coordx'] += 1
             return player
@@ -143,34 +145,34 @@ class Game:
         for furniture in furnitures:
             if furniture.get()['name'] == 'wheel':
                 while True:
-                    coordX = round(((len(self.__map) - 1) / 4) * 3)
-                    coordY = randint(2, len(self.__map[coordX]) - 3)
-                    if self.__map[coordX][coordY] == 1:
+                    coordX = round(((len(self.__mapShip) - 1) / 4) * 3)
+                    coordY = randint(2, len(self.__mapShip[coordX]) - 3)
+                    if self.__mapShip[coordX][coordY] == 1:
                         furniture.get()['coordX'] = coordX
                         furniture.get()['coordY'] = coordY
                         break
             if furniture.get()['name'] == 'anchor':
                 while True:
-                    coordX = randint(0, len(self.__map) - 1)
-                    coordY = randint(0, len(self.__map[coordX]) - 1)
-                    if self.__map[coordX][coordY] == 2:
+                    coordX = randint(0, len(self.__mapShip) - 1)
+                    coordY = randint(0, len(self.__mapShip[coordX]) - 1)
+                    if self.__mapShip[coordX][coordY] == 2:
                         furniture.get()['coordX'] = coordX
                         furniture.get()['coordY'] = coordY
                         break
             if furniture.get()['name'] == 'cannon':
                 while True:
-                    coordX = randint(0, len(self.__map) - 1)
-                    if (self.__map[coordX][0] and self.__map[coordX][len(self.__map[coordX]) - 1]) == 2:
+                    coordX = randint(0, len(self.__mapShip) - 1)
+                    if (self.__mapShip[coordX][0] and self.__mapShip[coordX][len(self.__mapShip[coordX]) - 1]) == 2:
                         side = randint(0, 1)
-                        coordY = 1 if side == 0 else (len(self.__map[coordX]) - 2)
-                        if self.__map[coordX][coordY] == 1:
+                        coordY = 1 if side == 0 else (len(self.__mapShip[coordX]) - 2)
+                        if self.__mapShip[coordX][coordY] == 1:
                             furniture.get()['coordX'] = coordX
                             furniture.get()['coordY'] = coordY
                             break
             if furniture.get()['name'] == 'rope':
-                coordX = randint(0, round((len(self.__map) - 1) / 3))
-                coordY = randint(0, len(self.__map[coordX]) - 1)
-                if self.__map[coordX][coordY] == 1:
+                coordX = randint(0, round((len(self.__mapShip) - 1) / 3))
+                coordY = randint(0, len(self.__mapShip[coordX]) - 1)
+                if self.__mapShip[coordX][coordY] == 1:
                     furniture.get()['coordX'] = coordX
                     furniture.get()['coordY'] = coordY
                     break
--- application/modules/game/Ship.py
+++ application/modules/game/Ship.py
@@ -3,9 +3,10 @@ class Ship:
         self.id = data['id']
         self.team = data['team']
         self.furniture = data['furniture']
+        self.globalCoord = 0
 
     def get(self):
-        return dict(id=self.id, teamId=self.team['id'], furniture=self.furniture)
+        return dict(id=self.id, teamId=self.team['id'], furniture=self.furniture, globalCoord=self.globalCoord)
 
     def getSelf(self):
-        return dict(id=self.id, team=self.team, furniture=self.furniture)
+        return dict(id=self.id, team=self.team, furniture=self.furniture, globalCoord=self.globalCoord)
